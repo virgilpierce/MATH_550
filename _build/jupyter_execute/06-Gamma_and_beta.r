7e42c3f856f5025babad45a5f873ed380505e04e
virgilpierce <virgilpierce@gmail.com>
2021-06-01 13:34:35 -0600

    Beta Distribution and Gamma Distribution Expected Values

--- _build/jupyter_execute/06-Gamma_and_beta.r
+++ _build/jupyter_execute/06-Gamma_and_beta.r
@@ -18,4 +18,26 @@ t <- c(0:(n*5))/n
 v <- dgamma(t, rate = lambda, shape = k)
 plot(t, v, type='l')
 
+# Note that 1/\beta is the rate from above; in this case then $\beta$ is called the scale
+
+n <- 100
+beta <- 1/0.9
+alpha <- 2
+t <- c(0:(n*5))/n
+v <- dgamma(t, scale = beta, shape = alpha)
+plot(t, v, type='l')
+
+v <- pgamma(t, scale = beta, shape=alpha)
+plot(t, v, type='l')
+
+n <- 500
+beta <- 4
+alpha <- 2
+t <- c(0:n)/n
+v <- dbeta(t, shape1=alpha, shape2 = beta)
+plot(t, v, type='l')
+
+v <- pbeta(t, shape1=alpha, shape2 = beta)
+plot(t, v, type='l')
+
 
