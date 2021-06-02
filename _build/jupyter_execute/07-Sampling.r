ef0793a14e19b66831dc56f47b10a45765ac93d8
virgilpierce <virgilpierce@gmail.com>
2021-06-02 10:21:52 -0600

    added an example of using CLT

--- _build/jupyter_execute/07-Sampling.r
+++ _build/jupyter_execute/07-Sampling.r
@@ -12,4 +12,20 @@ for (k in 1:50) {
 }
 hist(result)
 
+qnorm(0.025, 0, 1)
 
+# It's a compute so lets use it to keep track of a
+
+a = - qnorm(0.025, 0, 1)
+a
+
+# I always check that I used the inverse correctly and check the tails
+1 - pnorm(a, 0, 1)
+
+pnorm(-a, 0, 1)
+
+# we can also check the interior
+
+pnorm(a, 0, 1) - pnorm(-a, 0, 1)
+
+2.98 - a*0.32 / sqrt(10); 2.98 + a*0.32 / sqrt(10)
