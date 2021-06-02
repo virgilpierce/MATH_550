f3d4ee69e9df1fe8320c49b0c39604ee29edb1ec
virgilpierce <virgilpierce@gmail.com>
2021-06-02 09:48:34 -0600

    Central Limit Theorem Added

--- _build/jupyter_execute/07-Sampling.r
+++ _build/jupyter_execute/07-Sampling.r
@@ -1,7 +1,15 @@
-sample = rpois(5, 2)
+sample <- rpois(5, 2)
 sample
 
-sample = rpois(200, 2)
+sample <- rpois(200, 2)
 mean(sample)
+
+hist(sample, 20)
+
+result <- c()
+for (k in 1:50) {
+    result <- c(result, mean(rpois(5, 2)) )
+}
+hist(result)
 
 
